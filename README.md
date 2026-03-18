0dd02e221084969101aa86382bd8f58dcd82bee6
Daniel Koch <koch@terrestris.de>
2026-03-18 11:16:47 +0100

    feat: init landing page

---
+++ README.md
@@ -0,0 +1,8 @@
+# Material for the FOSSGIS 2026
+
+https://terrestris.github.io/fossgis2026/
+
+## How to use locally
+
+1. `npm ci`
+1. `npm start`
---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { resolve, join } from 'path';
+import { defineConfig } from 'vite';
+import { readdirSync } from 'fs';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const dirs = [
+  join(__dirname, 'talks')
+];
+
+const input = {
+  main: resolve(__dirname, 'index.html')
+};
+
+dirs.forEach(dir => {readdirSync(dir, {encoding: 'utf8', withFileTypes: true})
+  .filter(file => file.name.endsWith('.html'))
+  .forEach((file) => {
+    input[file.name] = resolve(dir, file.name);
+  });
+});
+
+export default defineConfig({
+  base: '',
+  // assetsInclude: ['**/*.svg'],
+  build: {
+    rollupOptions: {
+      input
+    }
+  }
+});
